122ec816c81053faa9441a29812306623e52c12b
Hayato <Hayato@Hayato-PC>
2019-04-23 13:33:28 +0900

    FirstCommit

---
+++ src/main/java/girlinaboxmod/Girl_in_a_BoxMod.java
@@ -0,0 +1,25 @@
+package girlinaboxmod;
+
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class Girl_in_a_BoxMod
+{
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
---
+++ src/main/java/girlinaboxmod/Reference.java
@@ -0,0 +1,12 @@
+package girlinaboxmod;
+
+public class Reference {
+	public static final String MODID = "girlinaboxmod";
+	public static final String NAME = "Girl in a Box Mod";
+	public static final String VERSION = "0.1";
+
+	public static final String CLIENT = "girlinaboxmod.proxy.ClientProxy";
+	public static final String SERVER = "girlinaboxmod.proxy.ServerProxy";
+
+	public static final int GUI_REGISTER_BLOCK = 1;
+}
